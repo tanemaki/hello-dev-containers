b6dd3bdce5aa15dda199ed1ee0514267f0862895
tanemaki <tanemaki@users.noreply.github.com>
2024-12-18 12:14:24 +0000

    Add task to greet using private library

--- tasks.py
+++ tasks.py
@@ -9,3 +9,11 @@ def say_hello(_context):
 @invoke.task
 def say_hello_to_somebody(_context, name):
     print(f"Hello, {name}!")
+
+
+@invoke.task
+def say_hello_using_private_library(_context):
+    # This is a private library defined under src/hello_dev_containers
+    import hello_dev_containers
+
+    print(hello_dev_containers.hello())
